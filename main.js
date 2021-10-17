009acd386bf226508619c19fde879662576d0053
yasminhammoud <yasmin.hammoud@correo.unimet.edu.ve>
2021-10-17 13:39:55 -0400

    i think we are done with this hero thing

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+/* --------------------------- SLIDESHOW  --------------------------- */
 const slideShowArray = ["./images/slide1.png" , "./images/slide2.png", "./images/slide3.png","./images/slide4.png","./images/slide5.png"]
 
 function showImage(index) {
     document.getElementById("image-to-display").src = slideShowArray[index];
 }
+/* ------------------------------------------------------------------ */
