4cf3eadd3911610acf1595fd7f4f6bbb9da19a26
yasminhammoud <yasmin.hammoud@correo.unimet.edu.ve>
2021-10-17 13:16:03 -0400

    the images are showing, finally!

---
+++ main.js
@@ -0,0 +1,5 @@
+const slideShowArray = ["./images/slide1.png" , "./images/slide2.png", "./images/slide3.png","./images/slide4.png","./images/slide5.png"]
+
+function showImage(index) {
+    document.getElementById("image-to-display").src = slideShowArray[index];
+}
